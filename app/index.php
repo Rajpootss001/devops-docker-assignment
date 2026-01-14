50cf5bd8ebfd3f36d0c8d24f113ed284a07e7c31
SATYAM SINGH RAJPOOT <163396283+Rajpootss001@users.noreply.github.com>
2026-01-14 11:12:28 +0530

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,3 +1,13 @@
 <?php
-echo "Docker setup working!";
+$host = "mysql";
+$db   = "testdb";
+$user = "root";
+$pass = "rootpass";
+
+try{
+    new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    echo "Database connection successful!";
+} catch(PDOException $e) {
+    echo "Database connection failed: ";
+}
 ?>
